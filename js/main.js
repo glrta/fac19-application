6cc9e2b7224e9f9874d37639ed07a60916a0a496
glrta <52718181+glrta@users.noreply.github.com>
2019-12-13 19:59:32 +0000

    Hide next and back buttons
    Carousel not infinite loop anymore.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,63 @@
 let currentSlide = "surname";
 const slideOrder = {
     "surname": {
-        docElement: document.getElementById("surname"),
+        slideElement: document.getElementById("surname"),
+        crumbElement: document.getElementById("crumb__home"),
         next: "about",
-        previous: "why",
+        previous: null,
     },
     "about": {
-        docElement: document.getElementById("about"),
+        slideElement: document.getElementById("about"),
+        crumbElement: document.getElementById("crumb__about"),
         next: "why",
         previous: "surname",
     },
     "why": {
-        docElement: document.getElementById("why"),
-        next: "surname",
+        slideElement: document.getElementById("why"),
+        crumbElement: document.getElementById("crumb__why"),
+        next: null,
         previous: "about",
     }
 }
 
 const nextButton = document.getElementById("next");
 nextButton.addEventListener("click", (event) => {
-    slideOrder[currentSlide].docElement.classList.add("d-none");
-    currentSlide = slideOrder[currentSlide].next;
-    slideOrder[currentSlide].docElement.classList.remove("d-none");
+    update(slideOrder[currentSlide].next);
+    
 });
 
 const backButton = document.getElementById("back");
-backButton.addEventListener("click", (event) => {
-    slideOrder[currentSlide].docElement.classList.add("d-none");
-    currentSlide = slideOrder[currentSlide].previous;
-    slideOrder[currentSlide].docElement.classList.remove("d-none");
+backButton.addEventListener("click", (event) => { 
+    update(slideOrder[currentSlide].previous);
+    
 });
+
+const update = (nextSlide) => {
+    disableCurrentPage();
+    currentSlide = nextSlide;
+    callNextSlide();
+    callArrowBehaviour();
+}
+
+const disableCurrentPage = () => {
+    slideOrder[currentSlide].slideElement.classList.add("d-none");
+    slideOrder[currentSlide].crumbElement.classList.remove("active");
+}
+
+const callNextSlide = () => {
+    slideOrder[currentSlide].slideElement.classList.remove("d-none");
+    slideOrder[currentSlide].crumbElement.classList.add("active");
+}
+
+const callArrowBehaviour = () => {
+    if ( !slideOrder[currentSlide].next ) {
+        nextButton.classList.add("d-none");
+    } else {
+        nextButton.classList.remove("d-none");
+    }
+    if ( !slideOrder[currentSlide].previous ) {
+        backButton.classList.add("d-none");
+    } else {
+        backButton.classList.remove("d-none");
+    }
+}
